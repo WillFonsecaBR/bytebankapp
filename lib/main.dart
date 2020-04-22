bce16762de94a3810bcf936f57c5a856a568e552
Willian <willian.alves2109@gmail.com>
2020-04-22 14:59:41 -0300

    Nova tela de Formulario de Transferencia, Finalizando aula 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,15 @@
+import 'package:bytebank/tranferencia.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: Column(
-            // A COLUNA DEIXA TODOS OS CARDS ALINHADOS UM ENCIMA DO OUTRO.
-            children: <Widget>[
-              //CRIASE UM FILHO NA COLUNA PARA QUE ELA POSSA PERMANECER ATIVA
-              Card(
-                //EXISTEM DIVERSOS TIPOS DE COMPLEMENTOS NO CARD
-                //PARA QUE O CARD SEJA ATIVO TEMOS QUE CRIAR UM CHILD.
-                //DENTRO DEST CHILD PODEMOS UTILIZAR A PROPRIEDADE DESEJADA.
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text(
-                    "100,00",
-                  ),
-                  subtitle: Text('Conta: 2030.20.1'),
-                ),
-              ),
-            ],
-          ),
-          appBar: AppBar(
-            title: const Text('Transferência'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
-        ),
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
     );
+  }
+}
--- lib/tranferencia.dart
+++ lib/tranferencia.dart
@@ -10,29 +10,37 @@ eventos que acontecem no aplicativo, enquanto o StatelessWidget
 não permite esse tipo de modificação.*/
 
 class Transferencia {
-  final String valor;
-  final String conta;
-
+  final double valor;
+  final int conta;
   Transferencia(
     this.valor,
     this.conta,
   );
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, Conta: $conta}';
+  }
 }
 
 class ItemTransferencia extends StatelessWidget {
-// Criando um objeto da classe trasferencia em uma variavel privada
+  //! Criando um objeto da classe trasferencia em uma variavel privada
   final Transferencia _transferencia;
 
-/*CONSTRUTOR = Passando os atributos da conta 
-  transferência para a classe item transferência.*/
+  /**
+   *! CONSTRUTOR => Passando os atributos da conta 
+   *! transferência para a classe item transferência.
+  **/
   ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      /*EXISTEM DIVERSOS TIPOS DE COMPLEMENTOS NO CARD
-      PARA QUE O CARD SEJA ATIVO TEMOS QUE CRIAR UM CHILD.
-      DENTRO DEST CHILD PODEMOS UTILIZAR A PROPRIEDADE DESEJADA.*/
+      /**
+       *? EXISTEM DIVERSOS TIPOS DE COMPLEMENTOS NO CARD
+       *? PARA QUE O CARD SEJA ATIVO TEMOS QUE CRIAR UM CHILD.
+       *? ENTRO DEST CHILD PODEMOS UTILIZAR A PROPRIEDADE DESEJADA.
+      **/
       child: ListTile(
         leading: Icon(Icons.monetization_on),
         title: Text(_transferencia.valor.toString()),
@@ -42,18 +50,107 @@ class ItemTransferencia extends StatelessWidget {
   }
 }
 
-class listaTransferencias extends StatelessWidget {
+class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // A COLUNA DEIXA TODOS OS CARDS ALINHADOS UM ENCIMA DO OUTRO.
-      children: <Widget>[
-        //CRIASE UM FILHO NA COLUNA PARA QUE ELA POSSA PERMANECER ATIVA
-        ItemTransferencia(Transferencia('100.00', '10.203040')),
-        ItemTransferencia(Transferencia('500.00', '10.203040')),
-        ItemTransferencia(Transferencia('600.00', '10.203040')),
-        ItemTransferencia(Transferencia('700.00', '10.203040')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferência'),
+      ),
+      body: Column(
+        //! A COLUNA DEIXA TODOS OS CARDS ALINHADOS UM ENCIMA DO OUTRO.
+        children: <Widget>[
+          //! CRIASE UM FILHO NA COLUNA PARA QUE ELA POSSA PERMANECER ATIVA
+          ItemTransferencia(Transferencia(100.00, 203040)),
+          ItemTransferencia(Transferencia(500.00, 203050)),
+          ItemTransferencia(Transferencia(500.00, 203060)),
+          ItemTransferencia(Transferencia(500.00, 203070))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  /**
+   *! CONTROLADOR DE TEXTO ==> TextEditingController 
+   *! PARA COLHER AS INFORMAÇÕES DO TEXTFILDS.
+   **/
+  final TextEditingController _controladorConta = TextEditingController();
+  final TextEditingController _controladorValor = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferencias'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              //! ESTILO DO TEXTO
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              //! O CONTROLER CAPTURA O TEXTO DO TEXTFIELD
+              controller: _controladorConta,
+              //! DECORAÇÃO DO TIPO DE IMPUT
+              decoration: InputDecoration(
+                labelText: 'Numero da Conta:',
+                hintText: '00.0000',
+              ),
+              //! TIPO DE TECLADO.
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              //! ESTILO DO TEXTO
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              //! O CONTROLER CAPTURA O TEXTO DO TEXTFIELD
+              controller: _controladorValor,
+              //! DECORAÇÃO DO TIPO DE IMPUT
+              decoration: InputDecoration(
+                  labelText: 'Valor:',
+                  hintText: '00,00',
+                  icon: Icon(
+                    //! INSERINDO ICONE DE MONETIZAÇÃO DENTRO DO TEXTFIELD
+                    Icons.monetization_on,
+                  )),
+              //! TIPO DE TECLADO.
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () {
+              //! MOSTRA UM PRINT NO DEBUG AO CLICAR NO BOTÃO.
+              // debugPrint('Clicou em confirmar');
+
+              //! COLHE AS INFORMAÇÕES INSERIDAS NOS TEXTFILDS
+              final int numeroConta = int.tryParse(_controladorConta.text);
+              final double valor = double.tryParse(_controladorValor.text);
+
+              //! INSERINDO OS VALORES NA CLASSE TRANSFERENCIA
+              if (numeroConta != null && valor != null) {
+                final transferenciaCriada = Transferencia(valor, numeroConta);
+                debugPrint('$transferenciaCriada');
+              } else {
+                debugPrint('NÃO FOI POSSIVEL REALIZAR A TRANSFERENCIA');
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
